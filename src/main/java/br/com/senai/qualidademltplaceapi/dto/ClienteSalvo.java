cb3ffe03e0f544ae8d5942059323ff1b168893a3
GABRIEL DOS SANTOS <gabriel_dos-santos@estudante.sc.senai.br>
2023-11-01 22:05:32 -0300

    iniciado implementação do dto

---
+++ src/main/java/br/com/senai/qualidademltplaceapi/dto/ClienteSalvo.java
@@ -0,0 +1,11 @@
+package br.com.senai.qualidademltplaceapi.dto;
+
+import jakarta.validation.constraints.NotNull;
+
+public class ClienteSalvo {
+	
+	@NotNull(message = "O email do cliente é obrigatório!")
+	String email;
+	
+	
+}
