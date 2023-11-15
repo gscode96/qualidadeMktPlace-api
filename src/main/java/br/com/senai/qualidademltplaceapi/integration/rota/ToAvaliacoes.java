8cecc6150e59f55d3e800d9d8a66d9ab5e29255f
Luiz Henrique Pereira <luuiz.pereira.correa@gmail.com>
2023-11-15 15:50:42 -0300

    continuação get de pedidos

--- src/main/java/br/com/senai/qualidademltplaceapi/integration/rota/ToAvaliacoes.java
+++ src/main/java/br/com/senai/qualidademltplaceapi/integration/rota/ToAvaliacoes.java
@@ -1,8 +1,5 @@
 package br.com.senai.qualidademltplaceapi.integration.rota;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
@@ -12,42 +9,72 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-import br.com.senai.qualidademltplaceapi.dto.PedidoSalvo;
 import br.com.senai.qualidademltplaceapi.integration.processor.ErrorProcessor;
 
 @Component
 public class ToAvaliacoes extends RouteBuilder {
 
-	@Value("${http://localhost9090/api}")
-	private String urlDeEnvio;
+	 @Value("${cadastros.url}")
+	    private String urlDeEnvio;
 
-	@Autowired
-	private ErrorProcessor errorProcessor;
+	    @Value("${cadastros.url.token}")
+	    private String urlDeToken;
 
-	// Adicione este atributo para armazenar a lista de PedidoSalvo
-	private List<PedidoSalvo> pedidoSalvos = new ArrayList<>();
+	    @Value("${cadastros.login}")
+	    private String login;
 
-	// Getter para a lista de PedidoSalvo
-	public List<PedidoSalvo> getPedidoSalvos() {
-		return pedidoSalvos;
-	}
+	    @Value("${cadastros.password}")
+	    private String senha;
 
-	@Override
-	public void configure() throws Exception {
-		// Faz o get na api de terceiro
-		from("direct:toApiPedidos").doTry().setHeader(Exchange.HTTP_METHOD, constant(HttpMethods.GET))
-				.setHeader(Exchange.CONTENT_TYPE, simple("application/json;charset=UTF-8")).process(new Processor() {
-					@Override
-					public void process(Exchange exchange) throws Exception {
-						String jsonBody = exchange.getMessage().getBody(String.class);
-						JSONObject jsonObject = new JSONObject(jsonBody) ;
-						exchange.getMessage().setBody(jsonObject);
-						
-						PedidoSalvo pedidoSalvo = exchange.getMessage().getBody(PedidoSalvo.class);
-						// Adicione o pedidoSalvo à lista
-						pedidoSalvos.add(pedidoSalvo);
-					}
-				}).toD(urlDeEnvio).doCatch(Exception.class).setProperty("error", simple("${exception}"))
-				.process(errorProcessor).end();
-	}
+	    @Autowired
+	    private ErrorProcessor errorProcessor;
+
+	    @Override
+	    public void configure() throws Exception {
+	        from("direct:toApiPedidos")
+	            .doTry()
+	                .process(new Processor() {
+	                    @Override
+	                    public void process(Exchange exchange) throws Exception {
+	                        String responseJson = exchange.getIn().getBody(String.class);
+	                        JSONObject jsonObject = new JSONObject(responseJson);
+	                        Integer idCliente = jsonObject.getInt("idCliente");
+	                        exchange.setProperty("idCliente", idCliente);
+	                    }
+	                })
+	                .to("direct:autenticarToken") // Call token verification route
+	                .setHeader(Exchange.HTTP_METHOD, constant(HttpMethods.GET))
+	                .setHeader(Exchange.CONTENT_TYPE, constant("application/json;charset=UTF-8"))
+	                .setHeader("Authorization", simple("Bearer ${exchangeProperty.token}"))
+	                //indica a rota a onde sera feita o get
+	                .toD(urlDeEnvio + "/pedidos ")
+	                .process(new Processor() {
+	                    @Override
+	                    public void process(Exchange exchange) throws Exception {
+	                        String responseJson = exchange.getIn().getBody(String.class);
+	                        JSONObject jsonObject = new JSONObject(responseJson);
+	                        exchange.getMessage().setBody(jsonObject.toString());
+	                    }
+	                })
+	            .doCatch(Exception.class)
+	                .setProperty("error", simple("${exception}"))
+	                .process(errorProcessor)
+	            .end();
+
+	        // verificação da rota do token
+	        from("direct:autenticarToken")
+	            .setHeader(Exchange.HTTP_METHOD, constant(HttpMethods.POST))
+	            .setHeader(Exchange.CONTENT_TYPE, constant("application/json"))
+	            .setBody(simple("{\"login\":\"${property.login}\",\"senha\":\"${property.senha}\"}"))
+	            .to(urlDeToken)
+	            .process(new Processor() {
+	                @Override
+	                public void process(Exchange exchange) throws Exception {
+	                    String responseJson = exchange.getIn().getBody(String.class);
+	                    JSONObject jsonObject = new JSONObject(responseJson);
+	                    String token = jsonObject.getString("token");
+	                    exchange.setProperty("token", token);
+	                }
+	            });
+	    }
 }
