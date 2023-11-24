afb43d0fba493024c389df13a7dc820e65406030
GABRIEL DOS SANTOS <gabriel_dos-santos@estudante.sc.senai.br>
2023-11-24 19:16:56 -0300

    Adicionado classe de testes

--- src/test/java/qualidademktplaceapi/getAvaliacaoTest.java
+++ src/test/java/qualidademktplaceapi/getAvaliacaoTest.java
@@ -1,19 +1,38 @@
 package qualidademktplaceapi;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
+import br.com.senai.qualidademltplaceapi.InitApp;
 import br.com.senai.qualidademltplaceapi.entity.AvaliacaoCliente;
-import br.com.senai.qualidademltplaceapi.service.AvaliacaoService;
+import br.com.senai.qualidademltplaceapi.entity.Ouvidoria;
+import br.com.senai.qualidademltplaceapi.service.impl.AvaliacaoSeviceImpl;
+import br.com.senai.qualidademltplaceapi.service.impl.OuvidoriaSeviceImpl;
 
+@SpringBootTest(classes = InitApp.class)
 public class getAvaliacaoTest {
 	
 	@Autowired
-	AvaliacaoService service;
+	AvaliacaoSeviceImpl avaliacaoService;
+	
+	@Autowired
+	OuvidoriaSeviceImpl ouvidoriaService;
 	
 	@Test
-	public void testGetPor(Integer id) {
-		AvaliacaoCliente avaliacaoCliente = service.buscarPorAvaliacao(id);
+	public void testGetPor() {
+	
+		AvaliacaoCliente avaliacaoCliente = avaliacaoService.buscarPorAvaliacao(10);
+		Assertions.assertNotNull(avaliacaoCliente);
+		
+	}
+	
+	@Test
+	public void testGetPorOuvidoria() {
+		
+		Ouvidoria ouvidoria = ouvidoriaService.buscarPorOuvidoria(1);
+		Assertions.assertNotNull(ouvidoria);
 		
 	}
 	
